aa242a4e56756c8413e9713ce415d502d153054c
Fernando Nicolás <fersanti2896@gmail.com>
2022-11-29 16:53:07 -0600

    Heredando interfaz en un struct en StructPrueba.cs

---
+++ Interfaces/StructPrueba.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Interfaces {
+    internal struct StructPrueba : IInterfaz2 {
+        public void miMetodoSegundaInterfaz() {
+            throw new NotImplementedException();
+        }
+    }
+}
