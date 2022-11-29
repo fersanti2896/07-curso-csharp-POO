3e427ebab9c27ee2e8ad5867a1a0c945737453aa
Fernando Nicolás <fersanti2896@gmail.com>
2022-11-29 16:51:24 -0600

    Interfaz en Interfaces/

---
+++ Interfaces/IInterfaz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Interfaces {
+    internal interface IInterfaz {
+        void miMetodo1();
+        string miMetodo2(int param1);
+        bool miPropiedad1 { get; set; }
+        int miPropiedad2 { get; }
+    }
+}
---
+++ Interfaces/Interfaz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Interfaces {
+    internal class Interfaz : IInterfaz, IInterfaz2 {
+        public bool miPropiedad1 { get; set; }
+
+        public int miPropiedad2 => 42;
+
+        public void miMetodo1() {
+            throw new NotImplementedException();
+        }
+
+        public string miMetodo2(int param1) {
+            throw new NotImplementedException();
+        }
+
+        public void miMetodoSegundaInterfaz() {
+            throw new NotImplementedException();
+        }
+    }
+}
