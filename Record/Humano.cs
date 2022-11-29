4b466dd4c19ca5187385391eb01a2e6ea826c5f8
Fernando Nicolás <fersanti2896@gmail.com>
2022-11-29 13:43:16 -0600

    Herencia en struct

---
+++ Record/Humano.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Record {
+    internal record Humano {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
---
+++ Record/Persona.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Record {
+    internal record Persona : Humano {
+        public int Edad { get; set; }
+    }
+}
